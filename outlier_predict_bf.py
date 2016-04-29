b598183ac4a75534e2b039d8e4f11d870e9ed0ab
root <root@kabam-datasci-michael.c.brave-monitor-91717.internal>
2016-04-29 18:58:02 +0000

    date changes

--- outlier_predict_bf.py
+++ outlier_predict_bf.py
@@ -13,8 +13,8 @@ import cPickle
 
 def main():
 	print('Querying ...')
-	start_date = datetime.date(2016, 1, 1)
-	for i in range(116): 
+	start_date = datetime.date(2016, 3, 4)
+	for i in range(53): 
    
 	    search_date = start_date + datetime.timedelta(-5) 
 	    
